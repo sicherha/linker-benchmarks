8942d8f0f13a510fdbe4d7b4af05e309bc2d8cd8
Christoph Erhardt <github@sicherha.de>
2025-03-23 22:49:44 +0100

    Make container script compatible with Docker
    Podman honours the `PODMAN_USERNS` environment variable; Docker ignores
    it.

--- container.sh
+++ container.sh
@@ -5,8 +5,8 @@ set -e -u
 docker build -t linker-benchmark .
 
 mkdir -p .ccache .mozbuild
-docker run -it --rm \
-  --security-opt label=disable --userns=keep-id \
+PODMAN_USERNS=keep-id docker run -it --rm \
+  --security-opt label=disable \
   -v "$PWD:/benchmarks:Z" \
   -v .ccache:/root/.ccache:Z \
   -w /benchmarks \
